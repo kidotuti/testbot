74b4b46adb687fb72f2c0076d3aa218ee7847822
Vũ Văn Tiến <tienvu.061298@gmail.com>
2018-12-30 17:09:15 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', message => {
 
     if (message.content === 'kido') {
 
-       message.reply('Có việc gì cứ ibox Kido, đừng ngại nhé!');
+       message.reply('Bạn cần Kido giúp gì nào? Cứ ibox đừng ngại nhé!');
 
        }
 
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
